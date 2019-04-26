7ca577402e2abaed8839b982b0559a0852fcaed3
jkingoliver <king@us.ibm.com>
2019-04-26 17:20:42 -0500

    cleanup

--- server/services/service-index.js
+++
@@ -1,8 +0,0 @@
-var IBMCloudEnv = require('ibm-cloud-env');
-var serviceManager = require('./service-manager');
-IBMCloudEnv.init();
-
-module.exports = function(){
-	require('./service-cloudant')(serviceManager);
-
-};
